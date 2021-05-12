6e28e8f03a7334d7dd7d7d4b46716f2940a73ce9
Artur Galimsky <artur.galimsky@gmail.com>
2021-05-12 13:40:48 +0500

    web project mvc configuration

---
+++ VirtualDeviceUDPWeb/Models/DeviceModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualDeviceUDPWeb.Models
+{
+    public class DeviceModel
+    {
+        public ushort Value1 { set; get; }
+        public ushort Value2 { set; get; }
+        public ushort UpLimit { set; get; }
+        public ushort LowLimit { set; get; }
+
+        public bool IsWithinLimits => (Value2 <= UpLimit && Value2 >= LowLimit);
+
+        public DeviceModel() { }
+
+        public override string ToString()
+        {
+            return $"Value1: {Value1}, Value2: {Value2}, UpLimit: {UpLimit}, LowLimit {LowLimit}";
+        }
+    }
+}
---
+++ VirtualDeviceUDPWeb/Models/DeviceRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualDeviceUDPWeb.Models
+{
+    public class DeviceRepository
+    {
+        private static DeviceRepository _sharedRepository = new DeviceRepository();
+        private Dictionary<int, DeviceModel> _devices = new Dictionary<int, DeviceModel>();
+
+        public static DeviceRepository SharedRepository => _sharedRepository;
+
+        public Dictionary<int, DeviceModel> Devices => _devices;
+
+        public DeviceRepository()
+        {
+            _devices.Add(1, new DeviceModel()
+            {
+                Value1 = 25,
+                Value2 = 15,
+                UpLimit = 225,
+                LowLimit = 25
+            });
+
+            _devices.Add(2, new DeviceModel()
+            {
+                Value1 = 55,
+                Value2 = 4,
+                UpLimit = 222,
+                LowLimit = 12
+            });
+        }
+    }
+}
